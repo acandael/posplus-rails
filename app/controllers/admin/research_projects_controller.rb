13fe55882aa09bc2d3e644aeb031814ee4ca7407
Anthony Candaele <anthony.candaele@webmove.be>
2014-07-02 17:24:10 +0200

    implement scenario admin clicks research project link and views research project details

--- app/controllers/admin/research_projects_controller.rb
+++ app/controllers/admin/research_projects_controller.rb
@@ -2,4 +2,8 @@ class Admin::ResearchProjectsController < ApplicationController
   def index
     @research_projects = ResearchProject.all
   end
+
+  def show
+    @research_project = ResearchProject.find(params[:id])
+  end
 end
--- spec/features/admin_handles_research_projects_spec.rb
+++ spec/features/admin_handles_research_projects_spec.rb
@@ -7,4 +7,13 @@ feature "Admin interacts with research projects" do
     visit admin_research_projects_path    
     expect(page).to have_content research_project.title 
   end
+
+  scenario "Admin clicks research project link and views research project details" do
+    research_project = ResearchProject.new(title: "research project 1", body: "this is the content")
+    research_project.save
+    visit admin_research_projects_path    
+    click_link research_project.title
+    expect(page).to have_css 'h1', text: research_project.title
+    expect(page).to have_css 'div', text: research_project.body
+  end
 end
