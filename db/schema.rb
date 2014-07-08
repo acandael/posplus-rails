02d31035fc1b5812af24bdb66826583dcdf9c8b5
Anthony Candaele <anthony.candaele@webmove.be>
2014-07-08 20:17:33 +0200

    add title field to researchers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708134157) do
+ActiveRecord::Schema.define(version: 20140708181223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140708134157) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "theme_projects", force: true do |t|
