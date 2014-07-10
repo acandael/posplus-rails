5ec9a48f787568c06ba3424612f8ae116b400f26
Anthony Candaele <anthony.candaele@webmove.be>
2014-07-10 09:10:55 +0200

    implement feature spec visitor visits researcher and sees courses for researcher

--- spec/features/visitor_sees_researchers_spec.rb
+++ spec/features/visitor_sees_researchers_spec.rb
@@ -16,4 +16,13 @@ feature "Visitor interacts with people page and" do
     find("a[href='/people/#{@researcher.id}']").click
     expect(page).to have_css 'h1', text: @researcher.name
   end
+
+  scenario "visitor clicks researcher and sees courses for researcher" do
+    course = Fabricate(:course)
+    @researcher.courses << course
+    @researcher.image = "spec/uploads/monk.jpg"
+    visit people_path
+    find("a[href='/people/#{@researcher.id}']").click
+    expect(page).to have_css 'li', text: course.title
+  end
 end
