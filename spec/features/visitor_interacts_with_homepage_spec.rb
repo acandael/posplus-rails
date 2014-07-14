442f501e32e596c383cd70e49ae37982af993014
Anthony Candaele <anthony.candaele@webmove.be>
2014-07-14 16:47:52 +0200

    fix issue with document.file.identifier

--- spec/features/visitor_interacts_with_homepage_spec.rb
+++ spec/features/visitor_interacts_with_homepage_spec.rb
@@ -4,7 +4,7 @@ feature 'Visitor interacts with homepage' do
   scenario 'and sees research themes' do
     @research_theme = Fabricate(:research_theme)
     visit home_path
-    expect(page).to have_css 'h2', text: @research_theme.title
+    expect(page).to have_css 'h3', text: @research_theme.title
     expect(page).to have_css 'p', text: @research_theme.description
   end
 end
