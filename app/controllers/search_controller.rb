e1ad8afe9f959e8bdbfcbfff777727aa068d709f
Anthony Candaele <anthony.candaele@webmove.be>
2014-08-14 19:53:41 +0200

    remove research projects from search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,8 @@ class SearchController < ApplicationController
     @search_term = params[:q]
     if params[:q].nil?
       @publications = []
-      @research_projects = []
     else
       @publications = Publication.search params[:q] 
-      @research_projects = ResearchProject.search params[:q]
     end
   end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,5 +1,5 @@
 module SearchHelper
   def number_of_results
-    @publications.size.to_i + @research_projects.size.to_i
+    @publications.size 
   end
 end
