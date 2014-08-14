3a06184a7f85fab5fb81ac14fa973f14b933eefb
Anthony Candaele <anthony.candaele@webmove.be>
2014-08-14 11:06:35 +0200

    implement search highlighting for research projects

--- app/models/research_project.rb
+++ app/models/research_project.rb
@@ -18,6 +18,37 @@ class ResearchProject < ActiveRecord::Base
 
   mount_uploader :image, ResearchProjectImageUploader
 
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'body']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em class="highlight">'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            reference: {}
+          }
+        }
+      }
+    )
+  end
+
 end
 
-ResearchProject.import # for auto sync model with elastic search
+# Delete the previous publication index in Elasticsearch
+Publication.__elasticsearch__.client.indices.delete index: Publication.index_name rescue nil
+
+# Create the new index with the new mapping
+Publication.__elasticsearch__.client.indices.create \
+  index: Publication.index_name,
+  body: { settings: Publication.settings.to_hash, mappings: Publication.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
+Publication.import(force: true)
+
