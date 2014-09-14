6fc3a21cb348dcc7059f7c7dfc9e461cbe361c33
Anthony Candaele <anthony.candaele@webmove.be>
2014-09-14 10:28:37 +0200

    add secrets.yml to linked_files in config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch, 'capistrano'
 
 set :deploy_to, '/home/deploy/posplus'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
