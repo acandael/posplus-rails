c80ec3f9dc4c4a444bffd1a3a656955973f15d69
Anthony Candaele <anthony.candaele@webmove.be>
2014-12-04 14:13:12 +0100

    refactor researchthemes listing on homepage to partial

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @research_themes = ResearchTheme.all
+    @researchthemes = ResearchTheme.all
     @newsitems = NewsItem.last(3)
     @features = Feature.all
   end
