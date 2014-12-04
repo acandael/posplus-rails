3fc2bab758f3cf141f9207f81d53373d02564e89
Anthony Candaele <anthony.candaele@webmove.be>
2014-12-04 14:09:18 +0100

    refactor news listing to partial

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     @research_themes = ResearchTheme.all
-    @news = NewsItem.last(3)
+    @newsitems = NewsItem.last(3)
     @features = Feature.all
   end
 end
